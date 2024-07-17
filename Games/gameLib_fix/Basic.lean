7e042c454b964b2f33acd1590943c7bb97471ec1
Happyves <yves-j@gmx.de>
2024-07-17 12:59:47 +0200

    nrjgwrgöj

--- Games/gameLib_fix/Basic.lean
+++ Games/gameLib_fix/Basic.lean
@@ -297,8 +297,34 @@ lemma Invariant_snd {p : ℕ → Prop}
     rw [← Turn_snd_step] at c
     exact ih n c (a c)
 
+lemma Invariant_snd' {p : ℕ → Prop}
+  (bh : p 2) (ih : ∀ turn : ℕ, Turn_snd turn → p turn → p (turn + 2)):
+  ∀ turn : ℕ, turn ≥ 2 → Turn_snd turn → p turn :=
+  by
+  intro t
+  apply Nat.twoStepInduction _ _ _ t
+  swap
+  · intro no nope
+    rw [Turn_snd] at nope
+    contradiction
+  · intro no
+    linarith
+  · intro n a _ b c
+    rw [← Turn_snd_step] at c
+    cases' n with m
+    · apply bh
+    · cases' m with k
+      · rw [Turn_snd] at c
+        contradiction
+      · apply ih
+        · apply c
+        · apply a _ c
+          rw [Nat.succ_eq_add_one]
+          linarith
 
---#exit
+
+
+-- # History
 
 inductive Hist_legal (ini : α) (f_law s_law : α → List β → (β → Prop))  : List β → Prop
 | nil : Hist_legal ini f_law s_law  []
@@ -377,12 +403,397 @@ def History_on_turn
             ⟨ act :: h.val , ⟨Hist_legal.cons h.val act (by rw [if_neg ] ; exact leg ; rw [Turn_not_fst_iff_snd] ; exact T') h.property.1, (by dsimp ; rw [h.property.2])⟩⟩
 
 
+lemma History_on_turn_length (ini : α) (f_law s_law : α → List β → (β → Prop))
+  (fst_strat : fStrategy ini f_law s_law)  (snd_strat : sStrategy ini f_law s_law) (t : Nat) :
+  (History_on_turn ini f_law s_law fst_strat snd_strat t).val.length = t :=
+  by
+  apply (History_on_turn ini f_law s_law fst_strat snd_strat t).property.2
 
--- lemma History_on_turn_length {α β : Type u}
---     (ini : α) (fst_strat snd_strat: Strategy α β) (t : Nat) :
---     (History_on_turn ini fst_strat snd_strat t).length = t :=
---   by
---   induction' t with t ih
---   · dsimp [History_on_turn]
---   · dsimp [History_on_turn]
---     split_ifs <;> {rw [List.length_cons, ih]}
+
+
+
+-- # Games
+
+structure Game (α β : Type u) extends Game_World α β where
+  /-- The first players strategy-/
+  fst_strat : fStrategy (toGame_World.init_game_state) toGame_World.fst_legal toGame_World.snd_legal
+  /-- The second players strategy-/
+  snd_strat : sStrategy (toGame_World.init_game_state) toGame_World.fst_legal toGame_World.snd_legal
+
+
+/-- Same as `Game`, but for a symmetric game world-/
+structure Symm_Game (α β : Type u) extends Symm_Game_World α β where
+  /-- The first players strategy-/
+  fst_strat : fStrategy (toSymm_Game_World.init_game_state) toSymm_Game_World.law toSymm_Game_World.law
+  /-- The second players strategy-/
+  snd_strat : sStrategy (toSymm_Game_World.init_game_state) toSymm_Game_World.law toSymm_Game_World.law
+
+
+
+/-- Build a `Game` from a `Symm_Game`-/
+def Symm_Game.toGame {α β : Type u} (g : Symm_Game α β) : Game α β :=
+ {toGame_World :=  Symm_Game_World.toGame_World (g.toSymm_Game_World)
+  fst_strat := g.fst_strat
+  snd_strat := g.snd_strat
+  }
+
+/-- ADD DOCS-/
+structure Game_wDraw (α β : Type u) extends Game_World_wDraw α β where
+  /-- The first players strategy-/
+  fst_strat : fStrategy (toGame_World.init_game_state) toGame_World.fst_legal toGame_World.snd_legal
+  /-- The second players strategy-/
+  snd_strat : sStrategy (toGame_World.init_game_state) toGame_World.fst_legal toGame_World.snd_legal
+
+
+@[simp]
+lemma Symm_Game.toGame_ini {α β : Type u} (g : Symm_Game α β) :
+  g.toGame.init_game_state = g.init_game_state :=
+  by
+  rfl
+
+@[simp]
+lemma Symm_Game.toGame_fst_win {α β : Type u} (g : Symm_Game α β) :
+  g.toGame.fst_win_states = g.win_states :=
+  by
+  rfl
+
+@[simp]
+lemma Symm_Game.toGame_snd_win {α β : Type u} (g : Symm_Game α β) :
+  g.toGame.snd_win_states = g.win_states :=
+  by
+  rfl
+
+@[simp]
+lemma Symm_Game.toGame_fst_trans {α β : Type u} (g : Symm_Game α β) :
+  g.toGame.fst_transition = g.transition :=
+  by
+  rfl
+
+@[simp]
+lemma Symm_Game.toGame_snd_trans {α β : Type u} (g : Symm_Game α β) :
+  g.toGame.snd_transition = g.transition :=
+  by
+  rfl
+
+@[simp]
+lemma Symm_Game.toGame_fst_legal {α β : Type u} (g : Symm_Game α β) :
+  g.toGame.fst_legal = g.law :=
+  by
+  rfl
+
+@[simp]
+lemma Symm_Game.toGame_snd_legal {α β : Type u} (g : Symm_Game α β) :
+  g.toGame.snd_legal = g.law :=
+  by
+  rfl
+
+@[simp]
+lemma Symm_Game.toGame_fst_strat {α β : Type u} (g : Symm_Game α β) :
+  g.toGame.fst_strat = g.fst_strat :=
+  by
+  rfl
+
+@[simp]
+lemma Symm_Game.toGame_snd_start {α β : Type u} (g : Symm_Game α β) :
+  g.toGame.snd_strat = g.snd_strat :=
+  by
+  rfl
+
+instance {α β : Type u} : Coe (Symm_Game α β) (Game α β) where
+  coe := Symm_Game.toGame
+
+
+@[simp]
+lemma Symm_Game.toSymm_Game_World_ini {α β : Type u} (g : Symm_Game α β) :
+  g.toSymm_Game_World.init_game_state = g.init_game_state :=
+  by
+  rfl
+
+
+
+@[simp]
+lemma Symm_Game.toGame_toWorld {α β : Type u} (g : Symm_Game α β):
+  g.toGame.toGame_World = g.toSymm_Game_World.toGame_World :=
+  by
+  dsimp [Symm_Game.toGame, Game.toGame_World, Symm_Game.toSymm_Game_World, Symm_Game_World.toGame_World]
+
+
+-- # History of games
+
+
+/-- History for a given turn, given a game world and stategies-/
+def Game_World.history_on_turn {α β : Type u} (g : Game_World α β)
+    (fst_strat : fStrategy g.init_game_state g.fst_legal g.snd_legal) (snd_strat : sStrategy g.init_game_state g.fst_legal g.snd_legal) :
+    (t : ℕ) → { hist // Hist_legal g.init_game_state g.fst_legal g.snd_legal hist ∧ List.length hist = t } :=
+    History_on_turn g.init_game_state g.fst_legal g.snd_legal fst_strat snd_strat
+
+def Symm_Game_World.history_on_turn {α β : Type u} (g : Symm_Game_World α β)
+    (fst_strat : fStrategy g.init_game_state g.law g.law) (snd_strat : sStrategy g.init_game_state g.law g.law) :
+    (t : ℕ) → { hist // Hist_legal g.init_game_state g.law g.law hist ∧ List.length hist = t } :=
+    Game_World.history_on_turn g.toGame_World fst_strat snd_strat
+
+
+def Game.history_on_turn {α β : Type _} (g : Game α β) :
+  (t : ℕ) → { hist // Hist_legal g.init_game_state g.fst_legal g.snd_legal hist ∧ List.length hist = t } :=
+  Game_World.history_on_turn g.toGame_World g.fst_strat g.snd_strat
+
+def Symm_Game.history_on_turn {α β : Type _} (g : Symm_Game α β) :
+  (t : ℕ) → { hist // Hist_legal g.init_game_state g.law g.law hist ∧ List.length hist = t } :=
+  Symm_Game_World.history_on_turn g.toSymm_Game_World g.fst_strat g.snd_strat
+
+
+lemma Game_World.history_on_turn_def {α β : Type u} (g : Game_World α β) :
+    g.history_on_turn = History_on_turn g.init_game_state g.fst_legal g.snd_legal :=
+    by
+    rfl
+
+-- TODO : port initial stuff
+
+
+-- # State of games
+
+def Game_World.state_on_turn {α β : Type _} (g : Game_World α β)
+    (fst_strat : fStrategy g.init_game_state g.fst_legal g.snd_legal) (snd_strat : sStrategy g.init_game_state g.fst_legal g.snd_legal) : ℕ → α
+| 0 => g.init_game_state
+| n+1 => let h := g.history_on_turn fst_strat snd_strat n
+         if T : Turn_fst (n+1)
+         then let T' : Turn_fst (List.length h.val + 1) := by rw [h.property.2] ; exact T ;
+              g.fst_transition g.init_game_state h (fst_strat h.val T' h.property.1)
+         else let T' : Turn_snd (List.length h.val + 1) := by rw [Turn_snd_iff_not_fst , h.property.2] ; exact T ;
+              g.snd_transition g.init_game_state h (snd_strat h.val T' h.property.1)
+
+def Game.state_on_turn {α β : Type _} (g : Game α β) : ℕ → α :=
+  Game_World.state_on_turn g.toGame_World g.fst_strat g.snd_strat
+
+def Symm_Game_World.state_on_turn {α β : Type _} (g : Symm_Game_World α β)
+  (fst_strat : fStrategy g.init_game_state g.law g.law) (snd_strat : sStrategy g.init_game_state g.law g.law) : ℕ → α :=
+  Game_World.state_on_turn g.toGame_World fst_strat snd_strat
+
+def Symm_Game.state_on_turn {α β : Type _} (g : Symm_Game α β) : ℕ → α :=
+  Symm_Game_World.state_on_turn g.toSymm_Game_World g.fst_strat g.snd_strat
+
+-- TODO : port initial stuff
+
+
+-- # Properties of History
+
+
+
+lemma mem_History_on_turn {α β : Type _}
+    (ini : α) (f_law s_law : α → List β → (β → Prop))
+    (f_strat : fStrategy ini f_law s_law)  (s_strat : sStrategy ini f_law s_law)
+    (turn : ℕ) (x : β) :
+    let H := History_on_turn ini f_law s_law f_strat s_strat ;
+    x ∈ (H (turn)).val ↔ (∃ t < turn, if T : (Turn_fst (t+1)) then x = ((f_strat (H t).val (by rw [History_on_turn_length] ; exact T) (H t).property.1).val) else  (x = (s_strat (H t).val (by rw [History_on_turn_length] ; rw [Turn_not_fst_iff_snd] at T ; exact T) (H t).property.1).val)) :=
+    -- for ↑, recall that `f_strat ini (H t)` is the action of turn `t+1`
+    by
+    intro H
+    apply @Nat.strong_induction_on (fun turn => x ∈ (H (turn)).val ↔ (∃ t < turn, if T : (Turn_fst (t+1)) then x = ((f_strat (H t).val (by rw [History_on_turn_length] ; exact T) (H t).property.1).val) else  (x = (s_strat (H t).val (by rw [History_on_turn_length] ; rw [Turn_not_fst_iff_snd] at T ; exact T) (H t).property.1).val))) turn
+    intro n ih
+    cases' n with z s
+    · dsimp [H, History_on_turn]
+      simp only [List.not_mem_nil, not_lt_zero', false_and, exists_const]
+    · dsimp [H, History_on_turn]
+      split_ifs with T
+      · constructor
+        · intro c
+          rw [List.mem_cons] at c
+          cases' c with f hi
+          · use z
+            use (by exact Nat.le.refl)
+            rw [dif_pos T]
+            exact f
+          · specialize ih z (by exact Nat.le.refl)
+            obtain ⟨t, tl, tp⟩ := ih.mp hi
+            use t
+            use (by apply lt_trans tl ; exact Nat.lt.base z)
+        · rintro ⟨t,⟨tl,tp⟩⟩
+          rw [List.mem_cons]
+          split_ifs at tp with f
+          · specialize ih z (by exact Nat.le.refl)
+            by_cases q : t < z
+            · right
+              rw [ih]
+              use t
+              use q
+              rw [dif_pos f]
+              exact tp
+            · have : t = z := by exact Nat.eq_of_lt_succ_of_not_lt tl q
+              --rw [this] at tp
+              left
+              convert tp
+              · apply this.symm
+              · apply this.symm
+              · apply this.symm
+              · apply this.symm
+          · specialize ih z (by exact Nat.le.refl)
+            by_cases q : t < z
+            · right
+              rw [ih]
+              use t
+              use q
+              rw [dif_neg f]
+              exact tp
+            · have : t = z := by exact Nat.eq_of_lt_succ_of_not_lt tl q
+              rw [this] at f
+              exfalso
+              exact f T
+      · constructor
+        · intro c
+          rw [List.mem_cons] at c
+          cases' c with f hi
+          · use z
+            use (by exact Nat.le.refl)
+            rw [dif_neg T]
+            exact f
+          · specialize ih z (by exact Nat.le.refl)
+            obtain ⟨t, tl, tp⟩ := ih.mp hi
+            use t
+            use (by apply lt_trans tl ; exact Nat.lt.base z)
+        · rintro ⟨t,⟨tl,tp⟩⟩
+          rw [List.mem_cons]
+          split_ifs at tp with f
+          · specialize ih z (by exact Nat.le.refl)
+            by_cases q : t < z
+            · right
+              rw [ih]
+              use t
+              use q
+              rw [dif_pos f]
+              exact tp
+            · have : t = z := by exact Nat.eq_of_lt_succ_of_not_lt tl q
+              rw [this] at f
+              exfalso
+              exact T f
+          · specialize ih z (by exact Nat.le.refl)
+            by_cases q : t < z
+            · right
+              rw [ih]
+              use t
+              use q
+              rw [dif_neg f]
+              exact tp
+            · have : t = z := by exact Nat.eq_of_lt_succ_of_not_lt tl q
+              --rw [this] at tp
+              left
+              convert tp
+              · apply this.symm
+              · apply this.symm
+              · apply this.symm
+              · apply this.symm
+
+
+
+
+lemma History_on_turn_nonempty_of_succ
+  (ini : α) (f_law s_law : α → List β → (β → Prop))
+  (f_strat : fStrategy ini f_law s_law)  (s_strat : sStrategy ini f_law s_law) {t : ℕ} :
+  (History_on_turn ini f_law s_law f_strat s_strat (t+1)).val ≠ [] :=
+  by
+  dsimp [History_on_turn]
+  split_ifs <;> decide
+
+
+lemma History_on_turn_zero
+  (ini : α) (f_law s_law : α → List β → (β → Prop))
+  (f_strat : fStrategy ini f_law s_law)  (s_strat : sStrategy ini f_law s_law) :
+  (History_on_turn ini f_law s_law f_strat s_strat 0).val = [] :=
+  by rfl
+
+lemma History_on_turn_getLast_fst_act (ini : α) (f_law s_law : α → List β → (β → Prop))
+  (f_strat : fStrategy ini f_law s_law)  (s_strat : sStrategy ini f_law s_law) :
+  ∀ t : ℕ, (History_on_turn ini f_law s_law f_strat s_strat (t+1)).val.getLast (by apply History_on_turn_nonempty_of_succ)
+    = f_strat [] (by dsimp ; decide) (by apply Hist_legal.nil):=
+  by
+  intro t
+  induction' t with t ih
+  · dsimp [History_on_turn]
+    simp_rw [dif_pos (show Turn_fst (0 + 1) from (by decide))]
+    dsimp [List.getLast]
+  · dsimp [History_on_turn] at *
+    by_cases  q : Turn_fst (Nat.succ t + 1)
+    · simp_rw [dif_pos q]
+      rw [List.getLast_cons]
+      · exact ih
+    · simp_rw [dif_neg q]
+      rw [List.getLast_cons]
+      · exact ih
+
+
+
+lemma History_eq_of_strat_strong_eq (ini : α) (f_law s_law : α → List β → (β → Prop))
+  (f_strat F_strat : fStrategy ini f_law s_law)  (s_strat S_strat : sStrategy ini f_law s_law)
+  (T : Nat)
+  (hf : ∀ hist : List β, (leg : Hist_legal ini f_law s_law hist) → (τ : Turn_fst (List.length hist + 1)) →  hist.length ≤ T → (f_strat hist τ leg).val = (F_strat hist τ leg).val)
+  (hs : ∀ hist : List β, (leg : Hist_legal ini f_law s_law hist) → (τ : Turn_snd (List.length hist + 1)) →  hist.length ≤ T → (s_strat hist τ leg).val = (S_strat hist τ leg).val) :
+  ∀ t ≤ (T+1), History_on_turn ini f_law s_law f_strat s_strat t = History_on_turn ini f_law s_law F_strat S_strat t :=
+  by
+  intro t tle
+  induction' t with t ih
+  · dsimp [History_on_turn]
+  · dsimp [History_on_turn]
+    by_cases q : Turn_fst (t + 1)
+    · rw [dif_pos q, dif_pos q]
+      specialize ih (by apply le_trans _ tle ; exact Nat.le.step Nat.le.refl)
+      --dsimp
+      simp_rw [ih]
+      congr 2
+      --simp_rw [ih]
+      specialize hf (History_on_turn ini f_law s_law f_strat s_strat t).val (History_on_turn ini f_law s_law f_strat s_strat t).property.1 (by rw [History_on_turn_length] ; exact q) (by rw [History_on_turn_length] ; exact Nat.lt_succ.mp tle)
+      convert hf using 1
+      congr 2
+      · simp_rw [ih]
+      · simp_rw [ih]
+      · apply heq_prop
+      · apply heq_prop
+    · rw [dif_neg q, dif_neg q]
+      specialize ih (by apply le_trans _ tle ; exact Nat.le.step Nat.le.refl)
+      simp_rw [ih]
+      congr 2
+      --simp_rw [ih]
+      specialize hs (History_on_turn ini f_law s_law f_strat s_strat t).val (History_on_turn ini f_law s_law f_strat s_strat t).property.1 (by rw [History_on_turn_length] ; exact q) (by rw [History_on_turn_length] ; exact Nat.lt_succ.mp tle)
+      convert hs using 1
+      congr 2
+      · simp_rw [ih]
+      · simp_rw [ih]
+      · apply heq_prop
+      · apply heq_prop
+
+
+lemma History_eq_of_strat_strong_eq' (ini : α) (f_law s_law : α → List β → (β → Prop))
+  (f_strat F_strat : fStrategy ini f_law s_law)  (s_strat S_strat : sStrategy ini f_law s_law)
+  (T : Nat)
+  (hf : ∀ hist : List β, (leg : Hist_legal ini f_law s_law hist) → (τ : Turn_fst (List.length hist + 1)) →  hist.length < T → (f_strat hist τ leg).val = (F_strat hist τ leg).val)
+  (hs : ∀ hist : List β, (leg : Hist_legal ini f_law s_law hist) → (τ : Turn_snd (List.length hist + 1)) →  hist.length < T → (s_strat hist τ leg).val = (S_strat hist τ leg).val) :
+  ∀ t ≤ T, History_on_turn ini f_law s_law f_strat s_strat t = History_on_turn ini f_law s_law F_strat S_strat t :=
+  by
+  intro t tle
+  induction' t with t ih
+  · dsimp [History_on_turn]
+  · dsimp [History_on_turn]
+    by_cases q : Turn_fst (t + 1)
+    · rw [dif_pos q, dif_pos q]
+      specialize ih (by apply le_trans _ tle ; exact Nat.le.step Nat.le.refl)
+      --dsimp
+      simp_rw [ih]
+      congr 2
+      --simp_rw [ih]
+      specialize hf (History_on_turn ini f_law s_law f_strat s_strat t).val (History_on_turn ini f_law s_law f_strat s_strat t).property.1 (by rw [History_on_turn_length] ; exact q) (by rw [History_on_turn_length] ; exact Nat.succ_le.mp tle)
+      convert hf using 1
+      congr 2
+      · simp_rw [ih]
+      · simp_rw [ih]
+      · apply heq_prop
+      · apply heq_prop
+    · rw [dif_neg q, dif_neg q]
+      specialize ih (by apply le_trans _ tle ; exact Nat.le.step Nat.le.refl)
+      simp_rw [ih]
+      congr 2
+      --simp_rw [ih]
+      specialize hs (History_on_turn ini f_law s_law f_strat s_strat t).val (History_on_turn ini f_law s_law f_strat s_strat t).property.1 (by rw [History_on_turn_length] ; exact q) (by rw [History_on_turn_length] ; exact Nat.succ_le.mp tle)
+      convert hs using 1
+      congr 2
+      · simp_rw [ih]
+      · simp_rw [ih]
+      · apply heq_prop
+      · apply heq_prop
