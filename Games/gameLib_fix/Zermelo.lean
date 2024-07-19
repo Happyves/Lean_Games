b3b67bc2fd8f1d1194b3998b81c7a10eaca8c7ef
Happyves <yves-j@gmx.de>
2024-07-19 21:41:33 +0200

    nrjgwrgöj

--- Games/gameLib_fix/Zermelo.lean
+++ Games/gameLib_fix/Zermelo.lean
@@ -13,33 +13,22 @@ lemma Game_World.not_has_WL (g : Game_World α β) (h : ¬ g.has_WL) :
   ·  exact Game_World.has_WL.ws h
 
 
-inductive History_good : (α) → (α → List β → (β → Prop)) → (α → List β → (β → Prop)) → (List β) → (β) → Prop where
-| mk (ini : α) (f_law s_law : α → List β → (β → Prop)) (hist : List β) (act : β) (leg : Hist_legal ini f_law s_law (act :: hist)) (good : ∃ next : β, History_good ini f_law s_law (act :: hist) next) : History_good ini f_law s_law hist act
-  -- neutral : (State_of_hist act :: hist).neutral
 
---set_option trace.Elab.inductive true in
---set_option trace.Kernel true in
-inductive test : Nat → Nat → Prop where
-| mk (k) (n) (h : ∃ m, 1 = 0 →  test (k+1) m) : test (k) n
+variable {α β : Sort _} in -- inside leads to massive problems
+mutual -- Remeber to thank Arthur Adjedj
+inductive History_good  (ini : α) (f_law s_law : α → List β → (β → Prop)) : List β → β → Prop where
+| mk hist act : Hist_legal ini f_law s_law (act :: hist) → auxExists ini f_law s_law hist act →  History_good ini f_law s_law hist act
 
-inductive test' : Nat → Nat → Prop where
-| mk (k) (n) (h : PSigma Nat ((fun q m : Nat => 1 = 0 →  test' (q) m) (k+1))) : test' (k) n
-
-
-mutual -- Thanks Arthur
-inductive test : Nat → Nat → Prop where
-| mk (k) (n)  : ExistsTest k → test k n
-
-inductive ExistsTest : Nat → Prop where
-| intro (m : Nat) : test (k+1) m → ExistsTest k
+inductive auxExists (ini : α) (f_law s_law : α → List β → (β → Prop)) : List β → β → Prop where
+| intro hist act (next : β)  :  History_good ini f_law s_law (act :: hist) next → auxExists ini f_law s_law hist act
 end
 
-#check Exists
 
 lemma Game_World.Zermelo (g : Game_World α β) :
   g.isWL → g.has_WL :=
   by
-  intro h
-  contrapose h
-  unfold Game_World.isWL
-  push_neg
+  sorry
+  -- intro h
+  -- contrapose h
+  -- unfold Game_World.isWL
+  -- push_neg
