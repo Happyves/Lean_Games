1453e1b3d2c5d555f2ce2e3df4f3e0061e9398bb
Happyves <yves-j@gmx.de>
2024-08-20 12:33:31 +0200

    wjirbgijere

--- Games/gameLib_fixfix/Stealing.lean
+++ Games/gameLib_fixfix/Stealing.lean
@@ -8,6 +8,82 @@ Author: Yves Jäckle.
 import Games.gameLib_fixfix.Zermelo
 
 
+
+
+structure Bait (g : zSymm_Game_World α β) (trap : β) : Prop where
+  leg_fst : g.law g.init_game_state [] trap
+  leg_imp : ∀ hist, ∀ act, g.law g.init_game_state (hist ++ [trap]) act → g.law g.init_game_state (hist) act
+  leg_imp' : ∀ hist, ∀ act, (Z : ¬ hist = []) → hist.getLast Z = trap → Turn_fst (hist.length + 1) → g.law g.init_game_state hist.dropLast act → g.law g.init_game_state hist act
+  leg_hist : ∀ hist, Hist_legal g.init_game_state g.law g.law (hist ++ [trap])  → Hist_legal g.init_game_state g.law g.law (hist)
+
+
+
+structure hist_match_sStrat (g : zSymm_Game_World α β) (ws : sStrategy g.init_game_state g.law g.law) (hist : List β) : Prop where
+  leg : Hist_legal g.init_game_state g.law g.law hist
+  --coin : ∀ t, (tl : t < hist.length) → (T : Turn_snd (t+1)) → hist.rget ⟨t, tl⟩ = ws (hist.rtake t) (by rw [List.length_rtake (le_of_lt tl)] ; exact T) (Hist_legal_rtake g.init_game_state g.law g.law hist t leg)
+
+
+
+open Classical
+
+noncomputable
+def stolen_strat (g : zSymm_Game_World α β)
+  (trap : β) (hb : Bait g trap) (ws : sStrategy g.init_game_state g.law g.law) : fStrategy g.init_game_state g.law g.law :=
+  fun h ht hl =>  if M : hist_match_sStrat g ws (h ++ [trap])
+                  then  let move := (ws (h ++ [trap]) (by rw [List.length_append, List.length_singleton, ← Turn_fst_snd_step] ; exact ht) M.leg)
+                        ⟨move.val, hb.leg_imp h move.val (move.prop)⟩
+                  else g.exStrat_fst g.hgp h ht hl
+
+noncomputable
+def pre_stolen_strat (g : zSymm_Game_World α β)
+  (trap : β) (hb : Bait g trap) (s_strat : sStrategy g.init_game_state g.law g.law) : fStrategy g.init_game_state g.law g.law :=
+  fun h ht hl =>  if Z : h = []
+                  then ⟨trap, (by rw [Z] ; exact hb.leg_fst)⟩
+                  else  if M : h.getLast Z = trap
+                        then  let move := s_strat h.dropLast (by rw [List.length_dropLast, Nat.sub_add_cancel, Turn_snd_fst_step] ; exact ht ; rw [Nat.succ_le, List.length_pos] ; exact Z) (by have := List.dropLast_append_getLast Z ; rw [M] at this ; apply hb.leg_hist ; rw [this] ; exact hl)
+                              ⟨move.val, hb.leg_imp' h move.val Z M ht move.prop⟩
+                        else  g.exStrat_fst g.hgp h ht hl
+
+
+
+#exit
+
+structure Stealing_condition (g : zSymm_Game_World α β) where
+  trap : β
+  hb : Bait g trap
+  wb : ∀ hist, Hist_legal g.init_game_state g.law g.law hist → (g.fst_win_states g.init_game_state (hist ++ [trap]) ↔ g.fst_win_states g.init_game_state hist)
+
+theorem Strategy_stealing (g : zSymm_Game_World α β) (s : Stealing_condition g) : g.is_fst_win :=
+  by
+  cases' g.Zermelo with F S
+  · exact F
+  · obtain ⟨ws, ws_prop⟩ := S
+    let sto_strat := stolen_strat g s.trap s.hb ws
+    use sto_strat
+    intro s_strat
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #exit
 
 
