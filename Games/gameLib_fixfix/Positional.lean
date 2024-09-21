b9e5c4ef49d31d164e434e308c1e8ba2383298d1
Happyves <yves-j@gmx.de>
2024-09-21 12:37:40 +0200

    fpiwrgnprgnwpr

--- Games/gameLib_fixfix/Positional.lean
+++ Games/gameLib_fixfix/Positional.lean
@@ -99,9 +99,10 @@ lemma Positional_Game_World_playable [Inhabited α] [DecidableEq α] [Fintype α
 
 
 
-lemma Positional_Game_World.decreasing_neutral [DecidableEq α] [Fintype α] (win_sets : Finset (Finset α)) (hist : List α) (act : α) :
+lemma Positional_Game_World.decreasing_neutral [DecidableEq α] [Fintype α] (win_sets : Finset (Finset α))
+  (hist Hist : List α)  (su : hist <:+ Hist) :
   let g := Positional_Game_World win_sets
-  Finset.filter (fun x => (State_from_history g.init_game_state g.fst_transition g.snd_transition (act :: hist)) x = 0) Finset.univ
+  Finset.filter (fun x => (State_from_history g.init_game_state g.fst_transition g.snd_transition (Hist)) x = 0) Finset.univ
   ⊆ Finset.filter (fun x => (State_from_history g.init_game_state g.fst_transition g.snd_transition (hist)) x = 0) Finset.univ :=
   by
   intro g
@@ -109,18 +110,24 @@ lemma Positional_Game_World.decreasing_neutral [DecidableEq α] [Fintype α] (wi
   simp_rw [Finset.mem_filter]
   rintro ⟨_,xdef⟩
   refine' ⟨Finset.mem_univ _, _ ⟩
-  dsimp [State_from_history, Positional_Game_World] at xdef
-  rw [ite_self] at xdef
-  cases' hist with y l
-  · dsimp [State_from_history, Positional_Game_World]
-  · dsimp [State_from_history, Positional_Game_World]
-    rw [ite_self]
+  cases' Hist with y l
+  · rw [ List.suffix_nil] at su
+    rw [su]
+    exact xdef
+  · dsimp [State_from_history, Positional_Game_World] at xdef
+    rw [ite_self] at xdef
     dsimp [PosGame_trans] at *
     split_ifs at xdef
     · contradiction
     · contradiction
     · rename_i main
-      rw [if_neg (by contrapose! main ; exact List.Mem.tail act main)]
+      cases' hist with z L
+      · dsimp [State_from_history, Positional_Game_World]
+      · dsimp [State_from_history, Positional_Game_World]
+        rw [ite_self]
+        unfold PosGame_trans
+        rw [if_neg (by contrapose! main ; exact List.mem_of_mem_suffix main su)]
+
 
 
 
@@ -134,7 +141,7 @@ lemma Positional_Game_World.strict_decreasing_neutral [DecidableEq α] [Fintype 
   ⊂ Finset.filter (fun x => (State_from_history g.init_game_state g.fst_transition g.snd_transition (hist)) x = 0) Finset.univ :=
   by
   intro g leg N
-  rw [Finset.ssubset_iff_of_subset (Positional_Game_World.decreasing_neutral win_sets hist act)]
+  rw [Finset.ssubset_iff_of_subset (Positional_Game_World.decreasing_neutral win_sets hist (act :: hist) (List.suffix_cons act hist))]
   use act
   simp_rw [Finset.mem_filter, Finset.mem_univ, true_and]
   constructor
@@ -156,6 +163,8 @@ lemma Positional_Game_World.strict_decreasing_neutral [DecidableEq α] [Fintype 
     exact List.Mem.head hist
 
 
+
+
 #check 1
 
 #check Game_World.state_on_turn
@@ -174,6 +183,111 @@ lemma Positional_Game_World.draw_states_iff [DecidableEq α] [Fintype α] (win_s
   intro g
   simp_rw [Finset.filter_eq_empty_iff, Finset.mem_univ, true_imp_iff]
 
+
+
+lemma Positional_Game_World.strict_decreasing_neutral_hists [DecidableEq α] [Fintype α] [Inhabited α] (win_sets : Finset (Finset α)) :
+  let g := Positional_Game_World win_sets ;
+  ∀ (f_strat : fStrategy g.init_game_state g.fst_legal g.snd_legal),
+  ∀ (s_strat : sStrategy g.init_game_state g.fst_legal g.snd_legal),
+  ∀ n,
+  let H := (History_on_turn g.init_game_state g.fst_legal g.snd_legal f_strat s_strat)
+  State_from_history_neutral_wDraw g.init_game_state g.fst_win_states g.snd_win_states g.draw_states (H n) →
+  Finset.filter (fun x => (State_from_history g.init_game_state g.fst_transition g.snd_transition (H (n+1))) x = 0) Finset.univ
+  ⊂ Finset.filter (fun x => (State_from_history g.init_game_state g.fst_transition g.snd_transition (H n)) x = 0) Finset.univ :=
+  by
+  intro g f_strat s_strat n H N
+  have := (H (n+1)).prop.1
+  by_cases T : Turn_fst (n+1)
+  · dsimp [H, History_on_turn]
+    rw [dif_pos T]
+    dsimp [H, History_on_turn] at this
+    rw [dif_pos T] at this
+    apply Positional_Game_World.strict_decreasing_neutral win_sets _ _ this N
+  · dsimp [H, History_on_turn]
+    rw [dif_neg T]
+    dsimp [H, History_on_turn] at this
+    rw [dif_neg T] at this
+    apply Positional_Game_World.strict_decreasing_neutral win_sets _ _ this N
+
+
+lemma Positional_Game_World.terminates [DecidableEq α] [Fintype α] [Inhabited α] (win_sets : Finset (Finset α))
+  (win_sets_nontrivial : ∅ ∉ win_sets) :
+  let g := Positional_Game_World win_sets ;
+  ∀ (f_strat : fStrategy g.init_game_state g.fst_legal g.snd_legal),
+  ∀ (s_strat : sStrategy g.init_game_state g.fst_legal g.snd_legal),
+  ∃ n, ¬ Game_World_wDraw.state_on_turn_neutral g f_strat s_strat (n+1) ∧
+    ∀ m ≤ n, Game_World_wDraw.state_on_turn_neutral g f_strat s_strat m :=
+  by
+  intro g f_strat s_strat
+  let N := {s | ∃ n,
+      let H := (History_on_turn g.init_game_state g.fst_legal g.snd_legal f_strat s_strat n)
+      s = Finset.filter (fun x => (State_from_history g.init_game_state g.fst_transition g.snd_transition H) x = 0) Finset.univ
+      ∧ State_from_history_neutral_wDraw g.init_game_state g.fst_win_states g.snd_win_states g.draw_states H}
+  have N_nonempty : Set.Nonempty N := by
+    use Finset.filter (fun x => (State_from_history g.init_game_state g.fst_transition g.snd_transition (History_on_turn g.init_game_state g.fst_legal g.snd_legal f_strat s_strat 0)) x = 0) Finset.univ
+    use 0
+    intro H
+    constructor
+    · rfl
+    · dsimp [H, History_on_turn, State_from_history_neutral_wDraw, Positional_Game_World]
+      constructor
+      · intro con
+        obtain ⟨oh,no,pe⟩ := con.2
+        dsimp [State_from_history] at pe
+        rw [Finset.filter_false_of_mem (fun _ _ => by decide), Finset.subset_empty] at pe
+        rw [pe] at no
+        exact win_sets_nontrivial no
+      · constructor
+        · intro con
+          obtain ⟨oh,no,pe⟩ := con.2
+          dsimp [State_from_history] at pe
+          rw [Finset.filter_false_of_mem (fun _ _ => by decide), Finset.subset_empty] at pe
+          rw [pe] at no
+          exact win_sets_nontrivial no
+        · intro con
+          apply con Inhabited.default
+          dsimp [State_from_history]
+  obtain ⟨n,ndef,ne⟩ := WellFounded.min_mem Finset.isWellFounded_ssubset.wf N N_nonempty
+  use n
+  constructor
+  · intro con
+    rw [Game_World_wDraw.state_on_turn_neutral_State_from_history_neutral] at con
+    apply @WellFounded.not_lt_min _ _ Finset.isWellFounded_ssubset.wf N N_nonempty
+        (Finset.filter (fun x => (State_from_history g.init_game_state g.fst_transition g.snd_transition (History_on_turn g.init_game_state g.fst_legal g.snd_legal f_strat s_strat (n+1))) x = 0) Finset.univ)
+        (by use n+1 ) --; intro H ; exact ⟨rfl, con⟩ )
+    rw [ndef]
+    apply Positional_Game_World.strict_decreasing_neutral_hists
+    apply ne
+  · intro m mln
+    rw [Game_World_wDraw.state_on_turn_neutral_State_from_history_neutral]
+
+
+
+
+
+
+-- TODO : show that hists mini in terms of suffix ; show that wld states of positional get maintaind
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#check instWellFoundedLTNatInstLTNat
+
+
+#check Nat.lt_wfRel.wf
+#check Finset.filter_eq_empty_iff
+
+#check Finset.isWellFounded_ssubset.wf
 
 -- follwo proof method of ↓
 #exit
